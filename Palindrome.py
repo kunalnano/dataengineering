c25e4fb9b2752747e8f4b1ede7ed8a6695edc95c
Al Sharma <kunalnano@icloud.com>
2019-09-21 11:20:49 -0400

    Tweet sentiment analyzer, palindrome
    Added new palindrome simpler elegant method without using recursion.
    
    Added code for performing static (batch) sentiment analysis of Twitter Stream

--- Palindrome.py
+++ Palindrome.py
@@ -1,1 +1,11 @@
-def reverse(s)
+def reverse(string):
+    string = string[::-1]
+    return string
+
+
+s = input('')
+
+if s == reverse(s):
+    print('Palindrome!')
+else:
+    print('Womp womp, not a palindrome')
